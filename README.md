73397647e728dd3064e672e587f915014c41e789
Dov Benyomin Sohacheski <b@kloud.email>
2024-03-18 09:49:21 +0200

    ✨ Enhanced entrypoint setup

--- README.md
+++ README.md
@@ -4,13 +4,12 @@
 
 ## Environment Variables
 
-| `env`                          | Default | Mode      |
-| ------------------------------ | ------- | --------- |
-| `CONTAINER_MANUAL_SETUP`       | ➖       | `*`       |
-| `CONTAINER_MODE`               | `"app"` | `*`       |
-| `CONTAINER_PORT`               | `8000`  | app       |
-| `CONTAINER_SCHEDULER_INTERVAL` | `60`    | scheduler |
-| `CONTAINER_WORKER_DELAY`       | `10`    | worker    |
-| `CONTAINER_WORKER_SLEEP`       | `5`     | worker    |
-| `CONTAINER_WORKER_TRIES`       | `3`     | worker    |
-| `CONTAINER_WORKER_TIMEOUT`     | `300`   | worker    |
+| `env`                      | Default | Mode   |
+| -------------------------- | ------- | ------ |
+| `CONTAINER_MANUAL_SETUP`   | ➖       | `*`    |
+| `CONTAINER_MODE`           | `"app"` | `*`    |
+| `CONTAINER_PORT`           | `8000`  | app    |
+| `CONTAINER_WORKER_DELAY`   | `10`    | worker |
+| `CONTAINER_WORKER_SLEEP`   | `5`     | worker |
+| `CONTAINER_WORKER_TRIES`   | `3`     | worker |
+| `CONTAINER_WORKER_TIMEOUT` | `300`   | worker |
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -8,39 +8,68 @@ set -e
 : "${CONTAINER_WORKER_SLEEP:=5}"
 : "${CONTAINER_WORKER_TIMEOUT:=300}"
 : "${CONTAINER_WORKER_TRIES:=3}"
-: "${CONTAINER_SCHEDULER_INTERVAL:=60}"
 : "${APP_ENV:=production}"
 
-ARTISAN="php -d variables_order=EGPCS /app/artisan"
+ARTISAN="php -d variables_order=EGPCS /laravel/artisan"
+
+_migrate() {
+  local count=0
+  local timeout=20
+
+  while [ $count -lt "${timeout}" ]; do
+    php -f /common/test_db_connection.php > /dev/null 2>&1
+
+    status=$?
+
+    if [ $status -eq 0 ]; then
+      echo "✅ Database connection successful."
+      break
+    fi
+
+    echo "⏱ Waiting on database connection, retrying... $((timeout - count)) seconds left"
+    count=$((count + 1))
+    sleep 1
+  done
+
+  if [ $count -eq "${timeout}" ]; then
+    echo "⛔ Database connection failed after multiple attempts."
+    exit 1
+  fi
+
+  echo "🚀 Running migrations..."
+  ${ARTISAN} migrate --force --isolated
+}
 
 _setup() {
   if [ -n "${CONTAINER_MANUAL_SETUP}" ]; then
-    echo DEBUG: Skipping setup...
+    echo "⏭: Skipping setup..."
 
     return
   fi
 
-  echo DEBUG: Preparing application...
+  _migrate
 
-  ${ARTISAN} storage:link || true
+  if [ -d "/laravel/app/public/storage" ]; then
+    echo "✅ Storage already linked..."
+  else
+    echo "🔐 Linking the storage..."
+    ${ARTISAN} storage:link
+  fi
 
-  ${ARTISAN} optimize  || true
-  ${ARTISAN} config:cache || true
-  ${ARTISAN} route:cache || true
-  ${ARTISAN} view:cache || true
-  ${ARTISAN} events:cache || true
-
-  ${ARTISAN} migrate --force || true
+  ${ARTISAN} config:cache
+  ${ARTISAN} events:cache
+  ${ARTISAN} route:cache
+  ${ARTISAN} view:cache
 }
 
 _run() {
   case "${CONTAINER_MODE}" in
     app)
-      echo INFO: Running octane...
+      echo "🚀 Running octane..."
       exec "${ARTISAN}" octane:frankenphp --host=0.0.0.0 --port="${CONTAINER_PORT}"
       ;;
     worker)
-      echo INFO: Running the queue...
+      echo "⏳ Running the queue..."
       exec "${ARTISAN}" queue:work -vv \
         --no-interaction \
         --tries="${CONTAINER_WORKER_TRIES}" \
@@ -49,18 +78,15 @@ _run() {
         --delay="${CONTAINER_WORKER_DELAY}"
       ;;
     horizon)
-      echo INFO: Running horizon...
+      echo "Running horizon..."
       exec "${ARTISAN}" horizon
       ;;
     scheduler)
-      while true; do
-        echo "INFO: Running scheduled tasks."
-        "${ARTISAN}" schedule:run --verbose --no-interaction &
-        sleep "${CONTAINER_SCHEDULER_INTERVAL}s"
-      done
+      echo "📆 Running scheduled tasks..."
+      exec "${ARTISAN}" schedule:work --verbose --no-interaction
       ;;
     *)
-      echo "Could not match the container mode [${CONTAINER_MODE}]"
+      echo "⛔ Could not match the container mode [${CONTAINER_MODE}]"
       exit 1
       ;;
   esac
